85608174c106828fabc9c9177ae90fd301f501f4
Elhay Efrat <elhayefrat@gmail.com>
2022-12-22 13:34:30 +0000

    test

--- lola.py
+++ lola.py
@@ -1,7 +1,5 @@
+"""Module providingFunction printing python version."""
 import streamlit as st
-
-
 st.title("Hello World")
-st.write("This is a test")
-
-
+for i in range(10):
+    st.write("This is a test")
